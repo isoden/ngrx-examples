a257ac4dcdc00c1b12cb3a7e8fb9357ddebb260a
isoden <isoda@maboroshi.biz>
2017-12-31 17:03:23 +0900

    style: beautify

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,21 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './containers/app/app.component';
-import { TopPageComponent } from './containers/top-page/top-page.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild([
-      {
-        path     : '',
-        component: TopPageComponent,
-      },
-    ]),
+    RouterModule,
   ],
   declarations: [
     AppComponent,
-    TopPageComponent,
   ],
 })
 export class CoreModule { }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {
+    path      : '',
+    redirectTo: '/counter',
+    pathMatch : 'full',
+  },
+
   // カウンターページ
   {
     path        : 'counter',
